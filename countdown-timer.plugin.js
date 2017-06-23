9361df9e9e722fbb755b9ca024102032a538e190
dbsolde <dbsolde@gmail.com>
2017-06-24 06:26:11 +0800

    Countdown Timer
    Countdown timer for Squarespace

---
+++ countdown-timer.plugin.js
@@ -0,0 +1,54 @@
+/**
+	plugin: Countdown Timer
+	version: 01
+	website: Squareplugs.com
+	description: Countdown timer for Squarespace
+*/
+
+function initCountdownTimer(t) {
+  void 0 === t ? console.error("Countdown timer parameters is undefined.") : 
+	(initializeClock(t), $(window).on("popstate", function (i, t) {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			initializeClock(t), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			initializeClock(t), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function initializeClock(time) {
+	var clock = document.getElementById('timer');
+	var daysSpan = clock.querySelector('.days');
+	var hoursSpan = clock.querySelector('.hours');
+	var minutesSpan = clock.querySelector('.minutes');
+	var secondsSpan = clock.querySelector('.seconds');
+	function updateClock() {
+		var t = getTimeRemaining(time);
+		daysSpan.innerHTML = t.days;
+		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		if (t.total <= 0) {
+			clearInterval(timeinterval);
+		}
+	}
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+}
+
+function getTimeRemaining(time) {
+	var t = Date.parse(time) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}

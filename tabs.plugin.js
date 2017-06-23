945c91c8954730c17b9738fc2aa1be7b04f335b4
dbsolde <dbsolde@gmail.com>
2017-06-24 07:29:29 +0800

    Tabs
    Tabs for Squarespace

---
+++ tabs.plugin.js
@@ -0,0 +1,48 @@
+/**
+	plugin: Tabs
+	version: 01
+	website: Squareplugs.com
+	description: Tabs for Squarespace
+*/
+
+function initTabs(a,v){
+	void 0 === a ? console.error("Tabs parameters is undefined.") : 
+	(tabsFn(a,v), $(window).on("popstate", function (i, t) {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			tabsFn(a,v), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			tabsFn(a,v), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function tabsFn(tab,version){
+	if(version==1){
+		tabCurrState(tab);
+	}else{
+		tabCurrState(tab);
+		$("ul li").click(function(e) {
+		  if ($(this).hasClass('slider')) {
+		    return;
+		  }
+		  var whatTab = $(this).index();
+		  var howFar = 88 * whatTab;
+		  $(".slider").css({
+		    left: howFar + "px"
+		  });
+		});
+	}
+}
+
+function tabCurrState(tab){
+	var tab_class = $('ul.'+tab+' li');
+	tab_class.click(function(){
+		var tab_id = $(this).attr('data-tab');
+		tab_class.removeClass('current');
+		$('.tab-content').removeClass('current');
+		$(this).addClass('current');
+	    $("#"+tab_id).addClass('current');
+	});
+}

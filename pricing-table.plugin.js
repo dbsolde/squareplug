2af150ad06e266ee7d288e49b944b541e10082a4
dbsolde <dbsolde@gmail.com>
2017-06-22 19:32:57 -0700

    Pricing Table
    Pricing Table for Squarespace

---
+++ pricing-table.plugin.js
@@ -0,0 +1,36 @@
+/**
+	plugin: Pricing Table
+	version: 01
+	website: Squareplugs.com
+	description: Pricing Table for Squarespace
+*/
+function initPricingTable(a) {
+  void 0 === a ? console.error("Pricing table parameters is undefined.") : 
+	(pricingTableFn(a), $(window).on("popstate", function (i, t) {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			pricingTableFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			pricingTableFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function pricingTableFn(item){
+	$('.' + item).hover(function() {
+		if ($(this).hasClass('default')) {
+			$('.default').addClass('active');
+		} else {
+			$(this).addClass("active");
+			$('.default').removeClass('active');
+		}
+	}, function() {
+		if ($(this).hasClass('default')) {
+			$(this).addClass("active");
+		} else {
+			$(this).removeClass("active");
+			$('.default').addClass('active');
+		}
+	});
+}

d7b5704b84b42854dd97be4e40828d32342572c5
dbsolde <dbsolde@gmail.com>
2017-06-27 03:14:21 +0800

    Simple Tab
    Simple Tab for Squarespace

---
+++ simple-tab.plugin.js
@@ -0,0 +1,30 @@
+/**
+	plugin: Simple Tab
+	version: 01
+	website: Squareplugs.com
+	description: Simple Tab for Squarespace
+*/
+
+function initTabs(a){
+	void 0 === a ? console.error("Simple Tab parameters is undefined.") : 
+	(tabsFn(a), $(window).on("popstate", function (i, t) {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			tabsFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			tabsFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function tabsFn(tab){
+	var tab_class = $('ul.'+tab+' li');
+	tab_class.click(function(){
+		var tab_id = $(this).attr('data-tab');
+		tab_class.removeClass('current');
+		$('.tab-content').removeClass('current');
+		$(this).addClass('current');
+	    $("#"+tab_id).addClass('current');
+	});	
+}

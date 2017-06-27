ad5040cd0cc4709a3a7b8f7a82efc06c25ac6387
dbsolde <dbsolde@gmail.com>
2017-06-28 06:59:41 +0800

    Update tab-slider.plugin.js

--- tab-slider.plugin.js
+++ tab-slider.plugin.js
@@ -5,41 +5,51 @@
 	description: Tab slider Squarespace
 */
 
-function initsliderTab(a){
-	void 0 === a ? console.error("Slider Tab parameters is undefined.") : 
-	(sliderTab(a), $(window).on("popstate", function (i, t) {
-		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
-			sliderTab(a), $(this).off("DOMSubtreeModified")
-		})
-	}), $("body").on("click", "a", function () {
-		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
-			sliderTab(a), $(this).off("DOMSubtreeModified")
-		})
-	}))
+function initsliderTab(a) {
+	void 0 === a ? console.error("Slider Tab parameters is undefined.") :
+		(sliderTab(a), $(window).on("popstate", function (i, t) {
+			$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+				sliderTab(a), $(this).off("DOMSubtreeModified")
+			})
+		}), $("body").on("click", "a", function () {
+			$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+				sliderTab(a), $(this).off("DOMSubtreeModified")
+			})
+		}))
 }
 
-function sliderTab(tab){
+function sliderTab(tab) {
 	var leftPos, newWidth;
 	var tab_class = $('ul.slider-tab li');
-	tab_class.click(function(){
-			var tab_id = $(this).attr('data-tab');
-			$('ul.slider-tab li').removeClass('slider-tab-current');
-			$('.slider-tab-content').removeClass('slider-tab-current');
-			$(this).addClass('slider-tab-current');
-		    $("#"+tab_id).addClass('slider-tab-current');
-		});	
+	tab_class.click(function () {
+		var tab_id = $(this).attr('data-tab');
+		$('ul.slider-tab li').removeClass('slider-tab-current');
+		$('.slider-tab-content').removeClass('slider-tab-current');
+		$(this).addClass('slider-tab-current');
+		$("#" + tab_id).addClass('slider-tab-current');
+	});
 
-	$("ul.slider-tab").append("<li class='"+tab+"'></li>");
-    var currentWidth = $(".current_page_item").outerWidth();
-    $("."+tab).css({
+	$("ul.slider-tab").append("<li class='" + tab + "'></li>");
+	var currentWidth = $(".current_page_item").outerWidth();
+	$("." + tab).css({
 		width: currentWidth
 	});
-    tab_class.find("span").click(function (e) {
+	tab_class.find("span").click(function (e) {
 		var leftPos = $(this).position().left;
 		var newWidth = $(this).parent().width();
-		$("."+tab).stop().animate({
-		    left: leftPos,
-		    width: newWidth
+		$("." + tab).stop().animate({
+			left: leftPos,
+			width: newWidth
 		});
-    });
+	});
+	var drawer_class = $(".tab-drawer-heading");
+	drawer_class.click(function () {
+		$(".slider-tab-content").hide();
+		var d_activeTab = $(this).attr("data-tab");
+		$("#" + d_activeTab).fadeIn();
+		drawer_class.removeClass("active-accordion");
+		$(this).addClass("active-accordion");
+		$("ul.tabs li").removeClass("current");
+		$("ul.tabs li[data-tab^='" + d_activeTab + "']").addClass("current");
+	});
 }

e6b61a46466959b401a4e42b9fe7172954481283
dbsolde <dbsolde@gmail.com>
2017-06-28 06:34:24 +0800

    Update simple-tab.plugin.js

--- simple-tab.plugin.js
+++ simple-tab.plugin.js
@@ -26,5 +26,15 @@ function tabsFn(tab){
 		$('.tab-content').removeClass('current');
 		$(this).addClass('current');
 	    $("#"+tab_id).addClass('current');
-	});	
+	});
+	var drawer_class = $(".tab-drawer-heading");
+	drawer_class.click(function () {
+		$(".tab-content").hide();
+		var d_activeTab = $(this).attr("data-tab");
+		$("#" + d_activeTab).fadeIn();
+		drawer_class.removeClass("active-accordion");
+		$(this).addClass("active-accordion");
+		$("ul.tabs li").removeClass("current");
+		$("ul.tabs li[data-tab^='" + d_activeTab + "']").addClass("current");
+	});
 }

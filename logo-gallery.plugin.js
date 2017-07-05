235a1ad401ac7f715d4d7e8c0d371577a124ef67
dbsolde <dbsolde@gmail.com>
2017-07-05 11:12:26 +0800

    Logo Gallery
    Logo Gallery for Squarespace

---
+++ logo-gallery.plugin.js
@@ -0,0 +1,53 @@
+/**
+	plugin: Logo Gallery
+	version: 01
+	website: Squareplugs.com
+	description: Logo Gallery for Squarespace
+*/
+
+function initLogoGallery(a, b) {
+	void 0 === a ? console.error("Logo gallery parameter is undefined.") :
+		(initSlickSlider(a, b), $(window).on("popstate", function (i, t) {
+			$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+				initSlickSlider(a, b), $(this).off("DOMSubtreeModified")
+			})
+		}), $("body").on("click", "a", function () {
+			$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+				initSlickSlider(a, b), $(this).off("DOMSubtreeModified")
+			})
+		}))
+}
+
+function initSlickSlider(slideShow, autoplay) {
+	$('.regular').slick({
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: slideShow,
+		autoplay: autoplay,
+		infinite: true,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: slideShow,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: slideShow
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: slideShow,
+				}
+			}
+		]
+	});
+}

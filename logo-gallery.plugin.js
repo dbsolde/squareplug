2772082dcad0b8ce0692bdd24f19705900e81328
dbsolde <dbsolde@gmail.com>
2017-07-05 11:55:16 +0800

    Update logo-gallery.plugin.js

--- logo-gallery.plugin.js
+++ logo-gallery.plugin.js
@@ -6,7 +6,7 @@
 */
 
 function initLogoGallery(a, b) {
-	void 0 === a ? console.error("Logo gallery parameter is undefined.") :
+	void 0 === a ? console.error("Logo gallery parameters is undefined.") :
 		(initSlickSlider(a, b), $(window).on("popstate", function (i, t) {
 			$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
 				initSlickSlider(a, b), $(this).off("DOMSubtreeModified")
@@ -28,24 +28,24 @@ function initSlickSlider(slideShow, autoplay) {
 		responsive: [{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: slideShow,
-					infinite: true,
-					dots: true
+				slidesToShow: 3,
+				slidesToScroll: slideShow,
+				infinite: true,
+				dots: true
 				}
 			},
 			{
 				breakpoint: 600,
 				settings: {
-					slidesToShow: 2,
-					slidesToScroll: slideShow
+				slidesToShow: 2,
+				slidesToScroll: slideShow
 				}
 			},
 			{
 				breakpoint: 480,
 				settings: {
-					slidesToShow: 1,
-					slidesToScroll: slideShow,
+				slidesToShow: 1,
+				slidesToScroll: slideShow,
 				}
 			}
 		]

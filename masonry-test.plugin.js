c8eb987b6d37a838f3320d38273ad1917d961f7f
dbsolde <dbsolde@gmail.com>
2017-07-17 07:05:22 +0800

    Update masonry-test.plugin.js

--- masonry-test.plugin.js
+++ masonry-test.plugin.js
@@ -1,3 +1,19 @@
+function initMasonryFadeIn() {
+    fadeIdOnScroll();
+    $(window).on("popstate", function (event, state) {
+        $('body div:last-child div:nth-child(3)').bind("DOMSubtreeModified", function(){
+            fadeIdOnScroll();
+            $(this).off("DOMSubtreeModified");
+        });
+    });
+    $('body').on('click', 'a', function() {
+        $('body div:last-child div:nth-child(3)').bind("DOMSubtreeModified", function(){
+            fadeIdOnScroll();
+            $(this).off("DOMSubtreeModified");
+        });
+    });
+}
+
 function fadeIdOnScroll(){
 $( window ).scroll(function() {
         $('.sqs-gallery-design-grid-slide').each( function(i){

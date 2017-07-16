2f0a5e5c79b677368a2247e4b6761c8a85f82a68
dbsolde <dbsolde@gmail.com>
2017-07-17 07:53:09 +0800

    Update masonry-test.plugin.js

--- masonry-test.plugin.js
+++ masonry-test.plugin.js
@@ -17,6 +17,11 @@ function initMasonryFadeIn() {
 function fadeIdOnScroll(){
 $( window ).scroll(function() {
         $('.sqs-gallery-design-grid-slide').each( function(i){
+            var marginWrapper = $(this).children('.margin-wrapper');
+            var anchor = marginWrapper.children('.image-slide-anchor');
+            var img = anchor.children('.thumb-image');
+            var imageSlideTitle = marginWrapper.children('.image-slide-title');
+            
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object ){
@@ -28,6 +33,25 @@ $( window ).scroll(function() {
                       'animation-duration': '1s',
                       '-webkit-animation-fill-mode': 'both',
                       'animation-fill-mode': 'both'
+                });
+                $(this).on('mouseover', function (){
+                    if($(this).hasClass('fadeIn')){
+                        imageSlideTitle.css('z-index', 99);
+                        anchor.css('z-index', 999);
+                        img.css({
+                            'opacity': 0,
+                            'transition': 'opacity 0.2s ease-out',
+                            'z-index': 999
+                        });
+                    }
+                }).on('mouseout', function() {
+                    if($(this).hasClass('fadeIn')){
+                        imageSlideTitle.css('z-index', -1);
+                        img.css({
+                            'opacity': 1,
+                            'transition': 'opacity 0.1s ease-in'
+                        });
+                    }      
                 });
             }
         });

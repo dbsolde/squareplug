b3888a7cf3b9d93f2f5956ea9b626608ce73e0b3
dbsolde <dbsolde@gmail.com>
2017-07-18 02:46:29 +0800

    Create set-image-fade-on-scroll.plugin.js

---
+++ set-image-fade-on-scroll.plugin.js
@@ -0,0 +1,63 @@
+/**
+* @plugin: Image Fade On Scroll
+* @version: 0.1
+* @website: Squareplugs.com
+* @description: Image Fade On Scroll Plugin for Squarespace
+* @author: Zeddicus Zu'l Zorander <jimenez.phillip.felicidario@gmail.com>
+*/
+function setImageFadeOnScroll(t) {
+	void 0 === t ? console.error("ImageFadeOnScroll Parameters is undefined.") : (callImageFadeOnScroll(t), $(window).on("popstate", function (i, n) {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			callImageFadeOnScroll(t), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
+			callImageFadeOnScroll(t), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function callImageFadeOnScroll(t) {
+	var i = {};
+	$("img").each(function (n, e) {
+		var a = $(this).attr("data-src");
+		if (void 0 !== a) {
+			var r = a.split("/"),
+				o = r[r.length - 1];
+			if (-1 !== t.indexOf(o)) {
+				var s = $(this).offset().left,
+					d = $(this).parent().parent().parent().parent().parent().parent().parent().width() / 2;
+				d > s ? $(this).parent().parent(".intrinsic").css({
+					"margin-left": "-" + $(this).width() / 3 + "px"
+				}) : $(this).parent().parent(".intrinsic").css({
+					"margin-left": $(this).width() / 3 + "px"
+				}), i[n] = $(this).attr("src"), $(this).parent().parent(".intrinsic").css({
+					"opacity": "0",
+					"width": "90%",
+					"height": "auto"
+				})
+			}
+		}
+	}), $(window).on("scroll", function () {
+		$("img").each(function (n, e) {
+			var a = $(this).parent().parent(".intrinsic"),
+				r = $(this).attr("data-src");
+			if (void 0 !== r) {
+				var o = r.split("/"),
+					s = o[o.length - 1];
+				if (-1 !== t.indexOf(s)) {
+					var d = $(this).offset().top + $(this).outerHeight(),
+						l = $(window).scrollTop() + $(window).height(),
+						c = ($(this).offset().left, $(this).parent().parent().parent().parent().parent().parent().parent().width() / 2, l - $(window).height() + $(window).height() / 2);
+					l > d && d > c && $("<img/>").on("load", function () {
+						a.css({
+							"transition": "margin-left 0.3s ease-in",
+							"-webkit-transition": "margin-left 0.3s ease-in",
+							"margin-left": "0"
+						}).fadeTo("slow", 1)
+					}).attr("src", i[n])
+				}
+			}
+		})
+	})
+}

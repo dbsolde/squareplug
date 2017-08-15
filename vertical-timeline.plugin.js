cdd53d43a4ca750fcd9b0d8e40fe4b769cabea18
dbsolde <dbsolde@gmail.com>
2017-08-16 02:48:47 +0800

    Update vertical-timeline.plugin.js

--- vertical-timeline.plugin.js
+++ vertical-timeline.plugin.js
@@ -4,23 +4,8 @@
 * @website: Squareplugs.com
 * @description: Vertical Timeline Plugin for Squarespace
 */
-function initVerticalTimeline() {
-	fadeOnScrollTimeline();
-	$(window).on("popstate", function (event, state) {
-		$('body div:last-child div:nth-child(3)').bind("DOMSubtreeModified", function () {
-			fadeOnScrollTimeline();
-			$(this).off("DOMSubtreeModified");
-		});
-	});
-	$('body').on('click', 'a', function () {
-		$('body div:last-child div:nth-child(3)').bind("DOMSubtreeModified", function () {
-			fadeOnScrollTimeline();
-			$(this).off("DOMSubtreeModified");
-		});
-	});
-}
 
-function fadeOnScrollTimeline() {
+jQuery(function() {
 	new Waypoint.Inview({
 		element: $('.tl:odd')[0],
 		enter: function (direction) {
@@ -60,4 +45,4 @@ function fadeOnScrollTimeline() {
 			$('.tl-4').removeClass('rotateInDownRight');
 		}
 	});
-}
+});

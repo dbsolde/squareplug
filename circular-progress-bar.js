9bac72761d23c95d5e3f9483908c82996576f758
Dennis S <dbsolde@gmail.com>
2017-09-08 01:06:35 +0800

    Update circular-progress-bar.js

--- circular-progress-bar.js
+++ circular-progress-bar.js
@@ -5,7 +5,7 @@
 * @description: Progress Bar Plugin for Squarespace
 * @author: Zeddicus Zu'l Zorander <jimenez.phillip.felicidario@gmail.com>
 */
-document.writeln('<script type="text/javascript" src="https://cdn.rawgit.com/dbsolde/squareplug/767a7326/jquery.easypiechart.min.js"></script>');
+$.getScript('https://cdn.rawgit.com/dbsolde/squareplug/767a7326/jquery.easypiechart.min.js', function() {
 eval(function (p, a, c, k, e, d) {
 	e = function (c) {
 		return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
@@ -29,3 +29,4 @@ eval(function (p, a, c, k, e, d) {
 	}
 	return p
 }('2 r(i){q 0===i?p.s("u x w."):(6(i),$(5).v(2(){6(i)}),$(5).f("n",2(o,t){$("c 9:j-8 9:k-8(3)").l("7",2(){6(i),$(4).m("7")})}),$("c").f("y","a",2(){$("c 9:j-8 9:k-8(3)").l("7",2(){6(i),$(4).m("7")})}))}2 6(i){$(".z").K(2(){h o=($(4).I("H-g"),$(4).J(".g"));F(""===o.G()){h t=$(4).B().A+$(4).C(),d=$(5).D()+$(5).b(),e=d-$(5).b()+$(5).b()/1.1;d>t&&e>t&&$(4).E(i)}})}', 47, 47, '||function||this|window|callCircularLoader|DOMSubtreeModified|child|div||height|body|||on|percent|var||last|nth|bind|off|popstate||console|void|runCircularLoader|log||Configuration|scroll|undefined|is|click|chart|top|offset|outerHeight|scrollTop|easyPieChart|if|text|data|attr|children|each'.split('|'), 0, {}))
+});

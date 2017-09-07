9021e264f41cab7211246e5ff4ec95d7b92d4983
Dennis S <dbsolde@gmail.com>
2017-09-08 01:10:59 +0800

    Update circular-progress-bar.js

--- circular-progress-bar.js
+++ circular-progress-bar.js
@@ -5,7 +5,7 @@
 * @description: Progress Bar Plugin for Squarespace
 * @author: Zeddicus Zu'l Zorander <jimenez.phillip.felicidario@gmail.com>
 */
-$.getScript('https://cdn.rawgit.com/dbsolde/squareplug/767a7326/jquery.easypiechart.min.js', function() {
+$.getScript('https://cdn.rawgit.com/dbsolde/squareplug/767a7326/jquery.easypiechart.min.js,https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js', function() {
 eval(function (p, a, c, k, e, d) {
 	e = function (c) {
 		return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))

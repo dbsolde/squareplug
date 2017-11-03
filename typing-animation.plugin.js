072f70935e0bc437b56ba6dac2287ae16c3e7b1c
Dennis S <dbsolde@gmail.com>
2017-11-03 14:19:45 +0800

    Update typing-animation.plugin.js

--- typing-animation.plugin.js
+++ typing-animation.plugin.js
@@ -5,25 +5,25 @@
     description: Typing animation for Squareplugs
     *credits to typed.js
 */
-function initTypingAnimation(strings,stypeSpeed,backSpeed,backDelay) {
+function initTypingAnimation(strings,typeSpeed,backSpeed,backDelay) {
     void 0 === strings ? console.error("Typing Animation parameters is undefined.") : 
-    (typingAnimationFn(strings,stypeSpeed,backSpeed,backDelay), $(window).on("popstate", function (i, t) {
+    (typingAnimationFn(strings,typeSpeed,backSpeed,backDelay), $(window).on("popstate", function (i, t) {
         $("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
-            typingAnimationFn(strings,stypeSpeed,backSpeed,backDelay), $(this).off("DOMSubtreeModified")
+            typingAnimationFn(strings,typeSpeed,backSpeed,backDelay), $(this).off("DOMSubtreeModified")
         })
     }), $("body").on("click", "a", function () {
         $("body div:last-child div:nth-child(3)").bind("DOMSubtreeModified", function () {
-            typingAnimationFn(strings,stypeSpeed,backSpeed,backDelay), $(this).off("DOMSubtreeModified")
+            typingAnimationFn(strings,typeSpeed,backSpeed,backDelay), $(this).off("DOMSubtreeModified")
         })
     }))
 }
 
-function typingAnimationFn(strings,stypeSpeed,backSpeed,backDelay){
+function typingAnimationFn(strings,typeSpeed,backSpeed,backDelay){
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.1/typed.min.js', function () {       
         $('#animate-strings').typed({
 		strings: strings,
-		typeSpeed: stypeSpeed,
-		backDelay: 1000,
+		typeSpeed: typeSpeed,
+		backDelay: backDelay,
             	backSpeed: backSpeed,
             	loop: true
 	});
